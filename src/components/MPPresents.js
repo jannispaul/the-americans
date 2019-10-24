2f20c5432e3c8d21b986a81b7a7def08dd71b7b7
Jannis <jannis@pixel-pink.de>
2019-10-24 18:22:21 +0200

    smaller mp presents bar

--- src/components/MPPresents.js
+++ src/components/MPPresents.js
@@ -1,19 +1,28 @@
 import React from "react"
 import styled from "styled-components"
-// import { device } from "../theme/breakpoints"
+import { device } from "../theme/breakpoints"
 import MPPresents from "../content/images/mp-presents.svg"
 
 const PresentedBy = styled.div`
-  height: 90px;
+  height: 55px;
   background: #2c2c2c;
   /* padding-top: 25px; */
   display: flex;
   justify-content: center;
   align-content: center;
 `
+const StyledImage = styled.img`
+  margin-top: 5px;
+  max-width: 100%;
+  width: 200px;
+  @media ${device.tablet} {
+    margin-top: 10px;
+  }
+`
+
 const MyStatelessComponent = props => (
   <PresentedBy>
-    <img src={MPPresents} alt="Mediapioneer presents Schriftzug"></img>
+    <StyledImage src={MPPresents} alt="Mediapioneer presents Schriftzug" />
   </PresentedBy>
 )
 
