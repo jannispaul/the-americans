5ebdd9bf3cc01f2cc8bdd167c332614c82f5be85
Jannis <jannis@pixel-pink.de>
2019-10-29 18:11:48 +0100

    fixed windows fonts

--- src/components/SmallLabel.js
+++ src/components/SmallLabel.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 const StyledSmall = styled.small`
   color: ${props => props.color || "#e6270a"};
   margin-bottom: 8px;
-  font-family: "AmericanTypewriter", "Courier";
+  font-family: "AmericanTypewriter", "Courier New", serif;
   color: #575757;
   letter-spacing: 1.33px;
 `
