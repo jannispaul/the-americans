9ee5b32db3d3ef914213186212328f0c33b5b746
Jannis <jannis@pixel-pink.de>
2020-01-26 18:13:45 +0100

    Added Mp Products

--- src/components/MPProducts.js
+++ src/components/MPProducts.js
@@ -2,6 +2,8 @@ import React from "react"
 import styled from "styled-components"
 import { device } from "../theme/breakpoints"
 import MBPodcastImage from "./ImageComponents/MBPodcastImage"
+import UeberstundePodcastImage from "./ImageComponents/UeberstundePodcastImage"
+import WallStreetWeeklyPodcastImage from "./ImageComponents/WallStreetWeeklyPodcastImage"
 // import MBNewsletterImage from "./ImageComponents/MBNewsletterImage"
 // import PioneerBlogImage from "./ImageComponents/PioneerBlogImage"
 // import PioneerOneImage from "./ImageComponents/PioneerOneImage"
@@ -21,36 +23,50 @@ const StyledSection = styled.section`
     padding: 80px 24px;
   }
 `
+
+// const StyledLogo = styled.img`
+//   max-width: 200px;
+//   @media ${device.tablet} {
+//     max-width: 300px;
+//   }
+// `
 const ProductContainer = styled.div`
   width: 100%;
+  max-width: 900px;
+  margin: auto;
   display: grid;
-  /* grid-template-columns: repeat(2, minmax(100px, 1fr)); */
-  margin-top: 24px;
+  margin-top: 32px;
   grid-gap: 12px;
   justify-content: center;
 
-  /* @media ${device.laptop} {
-    grid-template-columns: repeat(4, minmax(192px, 1fr));
-  } */
+  @media ${device.tablet} {
+    grid-template-columns: repeat(3, minmax(100px, 1fr));
+    /* grid-template-columns: repeat(4, minmax(192px, 1fr)); */
+    margin-top: 64px;
+  }
   a {
     text-decoration: none;
-    font-family: Avenir-Medium, "Open Sans", -apple-system, BlinkMacSystemFont, Arial,
-    sans-serif;;
+    font-family: Avenir-Medium, "Open Sans", -apple-system, BlinkMacSystemFont,
+      Arial, sans-serif;
     color: #2c2c2c;
     font-size: 14px;
     letter-spacing: 0.17px;
     line-height: 20px;
     margin-bottom: 40px;
     max-width: 240px;
-    margin: auto;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
 
     @media ${device.laptop} {
-      font-size: 22px;
+      font-size: 20px;
       letter-spacing: 0.27px;
       line-height: 30px;
     }
-
-    transition: 200ms transform ease;
+    & div {
+      transform: scale(1);
+      transition: 200ms transform ease;
+    }
     :hover {
       & div {
         transform: scale(1.03);
@@ -66,7 +82,6 @@ const PreHeadline = styled.p`
   color: #ff0045;
   text-align: center;
 `
-
 const MPProducts = props => (
   <StyledSection>
     <div>
@@ -81,30 +96,22 @@ const MPProducts = props => (
           <MBPodcastImage></MBPodcastImage>
           <p>Steingarts Morning Briefing Podcast</p>
         </a>
-        {/* <a
-          href="https://www.gaborsteingart.com/morning-briefing/"
+        <a
+          href="https://wall-street-weekly.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <MBNewsletterImage></MBNewsletterImage>
-          <p>Steingarts Morning Breifing Newsletter</p>
+          <WallStreetWeeklyPodcastImage></WallStreetWeeklyPodcastImage>
+          <p>Wall Street Weekly – mit Sophie Schimanski</p>
         </a>
         <a
-          href="https://mediapioneer.com/blog/"
+          href="https://überstunde.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <PioneerBlogImage></PioneerBlogImage>
-          <p>Pioneer Blog – von Michael Bröcker</p>
+          <UeberstundePodcastImage></UeberstundePodcastImage>
+          <p>Überstunde – mit Marina Weisband und Michael Bröcker</p>
         </a>
-        <a
-          href="https://mediapioneer.com/redaktionsschiff/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <PioneerOneImage></PioneerOneImage>
-          <p>Pioneer One – das Redaktionsschiff</p>
-        </a> */}
       </ProductContainer>
     </div>
   </StyledSection>
